c9c4e3ff287a5f3138ab3d3f4bb919ff05054a3b
Benjamin Winkler <benjiwinkler@yahoo.com>
2021-03-29 09:32:34 -0500

    Password generator done

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 // Assignment code here
 
+//Random number generator
+var ranNum = function (max) {
+  var returnValue = Math.floor(Math.random() * (max + 1));
+  return returnValue;
+}
+
 // Request password length
 var passwordLength = function () {
-  var length = 0
+  var length = window.prompt("Choose a password Length: 8 - 128.");
   while (length < 8 || length > 128) {
-    length = window.prompt(" Choose a password Length between 8 - 128.");
+    length = window.prompt("Please choose a number between 8 and 128.");
   };
   return length;
 }
@@ -12,62 +18,53 @@ var passwordLength = function () {
 // Generates a lower case letter
 var lowerCase = function () {
   var letter = "abcdefghijklmnopqrstuvwxyz";
-  return letter.charAt(Math.floor(Math.random() * 27));
+  return letter.charAt(ranNum(letter.length));
 }
 
 // Generates a upper case letter
 var upperCase = function () {
   var letter = lowerCase();
   letter = letter.toUpperCase();
-  return letter.charAt(Math.floor(Math.random() * 27));
+  return letter.charAt(ranNum(letter.length));
 }
 
 // Generates a numeric
 var numeric = function () {
-  var number = Math.floor(Math.random() * 10);
+  var number = ranNum(9);
   return number;
 }
 
-// Generates a special charaters
-var specialCharaters = function () {
-  var specialCharaters = " !#$%&'()*+,-./:;<=>?@[]^_`{|}~";
-  return specialCharaters.charAt(Math.floor(Math.random() * 32));
+// Generates a Special Characters
+var specialCharacter = function () {
+  var specialCharacter = " !#$%&'()*+,-./:;<=>?@[]^_`{|}~";
+  return specialCharacter.charAt(ranNum(specialCharacter.length));
 }
 
-// Select types
-var types = function () {
-  var lowercaseConfirm = window.confirm("Would you like to have Lowercase Letters?");
-  console.log(lowercaseConfirm);
-  var uppercaseConfirm = window.confirm("Would you like to have Uppercase Letters?");
-  console.log(uppercaseConfirm);
-  var specialCharatersConfirm = window.confirm("Would you like to have Special Charaters?");
-  console.log(specialCharatersConfirm);
-  var numericConfirm = window.confirm("Would you like to have Numbers?");
-  console.log(numericConfirm);
-
-  if (lowercaseConfirm && uppercaseConfirm && specialCharatersConfirm && numericConfirm) {
-    return 1;
-  }
-  else {
-    return 2;
-  }
-}
-
-var charater = function (type) {
+// Randomly selects character type
+var character = function (num, special, lower, upper) {
   var newCharater = "";
+  var type = ranNum(4);
+
   switch (type) {
     case 1:
-      newCharater = numeric();
+      if (num) {
+        newCharater = numeric();
+      }
       break;
     case 2:
-      newCharater = specialCharaters();
+      if (special) {
+        newCharater = specialCharacter();
+      }
       break;
     case 3:
-      newCharater = lowerCase();
+      if (lower) {
+        newCharater = lowerCase();
+      }
       break;
     case 4:
-      newCharater = upperCase();
-      console.log(newCharater);
+      if (upper) {
+        newCharater = upperCase();
+      }
       break;
   }
   return newCharater;
@@ -76,18 +73,44 @@ var charater = function (type) {
 
 // Generates password
 var generatePassword = function () {
+
   // Get password length
   var length = passwordLength();
-  var type = types();
   var password = "";
+  var typeChoosen = true;
+
+  // Get Charater Types
+  while (typeChoosen) {
+    var lowercaseConfirm = window.confirm("Would you like to have Lowercase Letters?");
+
+    var uppercaseConfirm = window.confirm("Would you like to have Uppercase Letters?");
+
+    var specialCharacterConfirm = window.confirm("Would you like to have Special Characters?");
+
+    var numericConfirm = window.confirm("Would you like to have Numbers?");
+    if (lowercaseConfirm || uppercaseConfirm || specialCharacterConfirm || numericConfirm) {
+      typeChoosen = false;
+    }
+    else {
+      window.alert("Please choose at least one character type.")
+    }
+  }
 
   // Add charaters until password length
   while (password.length < length) {
-    password = password + charater(type);
+    password = password + character(numericConfirm, specialCharacterConfirm, lowercaseConfirm, uppercaseConfirm);
   }
+
+  //Verify  selections in Console
   console.log("password length: " + password.length);
   console.log("length choosen: " + length);
-  console.log("password: " + password);
+  console.log("Lowercase: " + lowercaseConfirm);
+  console.log("Uppercase: " + uppercaseConfirm);
+  console.log("Special Characters: " + specialCharacterConfirm);
+  console.log("Numeric: " + numericConfirm);
+  console.log("Password: " + password);
+
+  // Returns password to HTML
   return password;
 }
 
